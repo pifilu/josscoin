ba4993f990f2e925a785a67d7ee8fdb116958c07
pzron <86353010+pzron@users.noreply.github.com>
2023-07-07 00:11:19 -0400

    Add files via upload

---
+++ keygen.js
@@ -0,0 +1,9 @@
+const EC = require("elliptic").ec;
+var ec = new EC("secp256k1");
+
+//generate keys
+var key = ec.genKeyPair();
+const privateKey = key.getPrivate("hex");
+const publicKey = key.getPublic("hex");
+
+console.log(privateKey, publicKey);
